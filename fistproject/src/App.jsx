d3033b46502f8994435403f6f500266f754596e5
imtiyazali3252-del <imtiyazali3252@gmail.com>
2026-03-17 07:40:43 +0000

    day three

--- fistproject/src/App.jsx
+++ fistproject/src/App.jsx
@@ -4,32 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const name="Imtiyaz"
+ const greet="Hello Everyone"
+ let x=10;
+ let y=30
+function App(){
+}
+  function fun(){
+    return "imtiyaz ali";
+  }
+function sum(a,b){
+  return a+b;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+ return(
+
+  <div>
+    <h1>project run</h1>
+    <h2>value of variable{name}</h2>
+    <h2>hgh</h2>
+    <h3>{x+y}</h3>
+    <h1>use a functon in jsx</h1>
+    <h2>{fun()}</h2>
+    <h3>use in parameterized at a function{sum(32,54)}</h3>
+  </div>
+ )
 }
 
 export default App
